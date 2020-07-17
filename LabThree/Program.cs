1c6141b0ca0a35f4c902623a91790a8f4b558b67
Ada <asmelliem@gmail.com>
2020-07-16 21:57:03 -0400

    Added code base for Lab 3.

--- LabThree/Program.cs
+++ LabThree/Program.cs
@@ -2,11 +2,73 @@
 
 namespace LabThree
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name: ");
+            var name = Console.ReadLine();
+            var proceed = "";
+            do
+            {
+                Console.WriteLine("Enter a whole number between 1 and 100");
+                var numberInput = Console.ReadLine();
+
+                var validNumber = int.TryParse(numberInput, out var number);
+
+                if (!validNumber || (number > 100 || number < 1))
+                {
+                    Console.WriteLine("You didn't enter a valid number");
+                    Console.WriteLine("");
+                    proceed = "Y";
+                    continue;
+                }
+
+                if (IsEven(number))
+                {
+                    Console.WriteLine(DetermineEvenOutput(number, name));
+                }
+                else
+                {
+                    Console.WriteLine(DetermineOddOutput(number, name));
+                }
+
+                Console.WriteLine("Would you like to continue? (y/n)");
+                proceed = Console.ReadLine().ToUpper();
+            } while (proceed == "Y");
+        }
+
+        public static bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public static string DetermineEvenOutput(int value, string name)
+        {
+            if (value >= 2 && value < 25)
+            {
+                return $"\nHello {name}\nEven and less than 25.";
+            }
+            else if (value >= 26 && value <= 60)
+            {
+                return $"\nHello {name}\nEven.";
+            }
+            else
+            {
+                return $"\nHello {name}\n{value} Even";
+            }
+        }
+
+        public static string DetermineOddOutput(int value, string name)
+        {
+            if (value > 60)
+            {
+                return $"\nHello {name}\n{value} Odd";
+            }
+            else
+            {
+                return $"\nHello {name}\n{value} Odd";
+            }
         }
     }
 }
